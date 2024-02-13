40bb14022d0e4b4d513551296aac4ebc2a2784df
sch1zo1d <roma.s-2012@yandex.ru>
2024-02-13 08:47:29 +0300

    feat: add metrics handler

--- Makefile
+++ Makefile
@@ -1,6 +1,4 @@
 all:
-	go test cmd/agent/*.go
-	go test cmd/server/*.go
 	go build -o cmd/server/server cmd/server/*.go
 	go build -o cmd/agent/agent cmd/agent/*.go 
 	./metricstest -test.v -test.run=^TestIteration1$ -agent-binary-path=cmd/agent/agent
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,77 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+
+	"github.com/gorilla/mux"
+)
+
+type MemStorage struct {
+	Data struct{
+		gauge GaugeMetric
+		counter CounterMetric
+	}
+}
+
+type gauge float64
+type counter int64
+
+type GaugeMetric map[string]gauge
+type CounterMetric map[string]counter
+
+type Storage interface {
+	Add()
+	Delete()
+}
+
+var (
+	mu sync.Mutex
+	db = MemStorage{
+    Data: struct {
+        gauge   GaugeMetric
+        counter CounterMetric
+    }{
+        gauge:   make(GaugeMetric),
+        counter: make(CounterMetric),
+    },
+}
+
+)
+
+func HandlerMetric(w http.ResponseWriter, req *http.Request){
+	if req.Method != http.MethodPost || req.Header.Get("content-type") != "text/plain"{
+		w.WriteHeader(http.StatusMethodNotAllowed)
+        return
+	}
+	vars := mux.Vars(req)
+	
+	mu.Lock()
+	badReq := 0
+	if vars["name"] == "" {w.WriteHeader(http.StatusNotFound); return}
+	switch vars["type"] {
+	case "counter": 
+		if val, err := strconv.ParseInt(vars["value"], 10, 64); err==nil {
+			db.Data.counter[vars["name"]] += counter(val)
+		} else {badReq = 1}
+	case "gauge": 
+		if val, err := strconv.ParseFloat(vars["value"], 64); err==nil {
+			db.Data.gauge[vars["name"]] = gauge(val)
+		} else {badReq = 1}
+	default: badReq = 1
+	}
+	if badReq == 1 {w.WriteHeader(http.StatusBadRequest); return}
+	mu.Unlock()
+	w.Header().Set("content-type", "text/plain")
+	w.Header().Set("charset", "utf-8")
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	// init
+	mux := mux.NewRouter()
+	mux.HandleFunc("/update/{type}/{name}/{value}", HandlerMetric)
+    log.Fatal(http.ListenAndServe(":8080", mux))
+}
