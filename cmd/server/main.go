a10e1abedb5896e957f7cdf3439efbe015347d28
sch1zo1d <roma.s-2012@yandex.ru>
2024-02-14 17:24:14 +0300

    fix: address

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -46,7 +46,7 @@ var (
 
 var flagRunAddr string
 func parseFlags() {
-    flag.StringVar(&flagRunAddr, "a", ":8080", "address and port to run server")
+    flag.StringVar(&flagRunAddr, "a", "localhost:8080", "address and port to run server")
     flag.Parse()
 
 	if envRunAddr := os.Getenv("ADDRESS"); envRunAddr != "" {
